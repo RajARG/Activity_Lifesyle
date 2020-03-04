a268acb1d18cb41ef99b238ac879a075f6133ba0
1707592 <gupta.amey@gmail.com>
2020-03-04 14:14:59 -0500

    3/4/2020

--- app/src/main/java/com/guptaamey/activitylifesyle/MainActivity.java
+++ app/src/main/java/com/guptaamey/activitylifesyle/MainActivity.java
@@ -7,9 +7,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import android.view.View;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
-    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0;
+    TextView a, b, c, d, e, f, g;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
 
@@ -17,9 +20,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        a = findViewById(R.id.onCreateCtr);
+        b = findViewById(R.id.onStartCtr);
+        c = findViewById(R.id.onResumeCtr);
+        d = findViewById(R.id.onStopCtr);
+        e = findViewById(R.id.onPauseCtr);
+        f = findViewById(R.id.onRestartCtr);
+        g = findViewById(R.id.onDestroyCtr);
+
+        a.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+        b.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+        c.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+        d.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+        e.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+        f.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+        g.setText("0, " + sharedPreferences.getInt( a.getId() + "", 0));
+
+        String[] counters = a.getText().toString().split(", ");
+
+        editor.remove(a.getId() + "");
+        editor.apply();
+
+        editor.putInt(a.getId() + "", Integer.parseInt(counters[1]) + 1);
+        editor.apply();
+
+        a.setText(Integer.parseInt(counters[0])+ 1);
+
     }
 
     @Override
@@ -51,4 +79,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
 }
